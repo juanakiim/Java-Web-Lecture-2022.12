c868997248c063a8e8dfbbf48d789e152d77f84e
juanakiim <juanakiim@naver.com>
2022-12-07 17:03:48 +0900

    first commit

---
+++ src/main/java/ch05/ThirdServlet.java
@@ -0,0 +1,48 @@
+package ch05;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ThirdServlet
+ */
+@WebServlet("/ch05/Third")
+public class ThirdServlet extends HttpServlet {
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.print("<!DOCTYPE html>");
+		out.print("<html lang=\"en\">");
+		out.print("<head>"); 
+		out.print("<meta charset=\"UTF-8\">");
+		out.print("<title>Insert title here</title>");
+		out.print("</head>");
+		out.print("<body>");
+		out.print("<h1>안녕하세요?</h1>");
+		out.print("<h1>Hello world</h1>");
+		out.print("</body>");		
+		out.print("</html>");		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
